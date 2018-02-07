d5a4144a820ca67381fd59339252379d8c8bb34a
Rolandas Valteris <rolandas.valteris@gmail.com>
2018-02-07 11:32:54 -0500

    case study 10. data validation - added alternative implementation for Check as an ADT

--- src/main/scala/sandbox/cats/casestudy/DataValidation.scala
+++ src/main/scala/sandbox/cats/casestudy/DataValidation.scala
@@ -3,12 +3,13 @@ package sandbox.cats.casestudy
 import cats.Semigroup
 import cats.Semigroupal
 import cats.syntax.either._
+import cats.syntax.semigroup._
 
-trait Check[E, A] { self =>
+trait CheckF[E, A] { self =>
 
   def apply(a: A): Either[E, A]
 
-  def and(that: Check[E, A])(implicit esemi: Semigroup[E]): Check[E, A] =
+  def and(that: CheckF[E, A])(implicit esemi: Semigroup[E]): CheckF[E, A] =
     (a: A) =>
       Semigroupal
         .map2(
@@ -17,5 +18,24 @@ trait Check[E, A] { self =>
         )((a0, a1) => a0)
         .toEither
 }
+
+sealed trait Check[E, A] {
+
+  def and(that: Check[E, A]): Check[E, A] = And(this, that)
+
+  def apply(a: A)(implicit se: Semigroup[E]): Either[E, A] = this match {
+    case Pure(f) => f(a)
+    case And(l, r) =>
+      (l(a), r(a)) match {
+        case (Left(e1), Left(e2))   => (e1 |+| e2).asLeft
+        case (Left(e), Right(a))    => e.asLeft
+        case (Right(a), Left(e))    => e.asLeft
+        case (Right(a1), Right(a2)) => a.asRight
+      }
+  }
+}
+
+final case class Pure[E, A](chk: A => Either[E, A]) extends Check[E, A]
+final case class And[E, A](l: Check[E, A], r: Check[E, A]) extends Check[E, A]
 
 object DataValidation {}
