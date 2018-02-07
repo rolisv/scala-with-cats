4206b6dc46f9d8c98a085655bcc2ecad3f7bca61
Rolandas Valteris <rolandas.valteris@gmail.com>
2018-02-06 19:03:58 -0500

    case study 9. map reduce - converted to use cats Foldable and Traverse - complete

--- src/main/scala/sandbox/cats/casestudy/MapReduce.scala
+++ src/main/scala/sandbox/cats/casestudy/MapReduce.scala
@@ -18,4 +18,19 @@ object MapReduce {
       .traverse(fa.grouped(batchSize))(fab => Future(foldMap(fab)(f)))
       .map(bs => foldMap(bs.toVector)(identity))
   }
+
+  import cats.Foldable
+  import cats.Traverse
+  import cats.instances.vector._
+  import cats.instances.future._
+
+  def parFoldMapCats[A, B: Monoid](fa: Vector[A])(f: A => B): Future[B] = {
+    val threadCount = sys.runtime.availableProcessors()
+    val batchSize = (fa.size - 1) / threadCount + 1
+    val fld = Foldable[Vector]
+
+    Traverse[Vector]
+      .traverse(fa.grouped(batchSize).toVector)(fab => Future(fld.foldMap(fab)(f)))
+      .map(bs => fld.combineAll(bs))
+  }
 }
