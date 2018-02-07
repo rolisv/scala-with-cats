2d4b0d845f09210f050363536e16cc8aad371ec6
Rolandas Valteris <rolandas.valteris@gmail.com>
2018-02-07 15:05:05 -0500

    case study 10. data validation - converted from Either to Validated and added 'or' combinator

--- src/main/scala/sandbox/cats/casestudy/DataValidation.scala
+++ src/main/scala/sandbox/cats/casestudy/DataValidation.scala
@@ -1,15 +1,14 @@
 package sandbox.cats.casestudy
 
-import cats.Semigroup
-import cats.Semigroupal
+import cats.data.Validated.{Invalid, Valid}
+import cats.{Semigroup, Semigroupal}
 import cats.syntax.either._
-import cats.syntax.semigroup._
 
 trait CheckF[E, A] { self =>
 
   def apply(a: A): Either[E, A]
 
-  def and(that: CheckF[E, A])(implicit esemi: Semigroup[E]): CheckF[E, A] =
+  def and(that: CheckF[E, A])(implicit se: Semigroup[E]): CheckF[E, A] =
     (a: A) =>
       Semigroupal
         .map2(
@@ -19,23 +18,31 @@ trait CheckF[E, A] { self =>
         .toEither
 }
 
+import cats.data.Validated
+import cats.syntax.semigroup._
+
 sealed trait Check[E, A] {
 
   def and(that: Check[E, A]): Check[E, A] = And(this, that)
+  def or(that: Check[E, A]): Check[E, A] = Or(this, that)
 
-  def apply(a: A)(implicit se: Semigroup[E]): Either[E, A] = this match {
-    case Pure(f) => f(a)
-    case And(l, r) =>
-      (l(a), r(a)) match {
-        case (Left(e1), Left(e2))   => (e1 |+| e2).asLeft
-        case (Left(e), Right(a))    => e.asLeft
-        case (Right(a), Left(e))    => e.asLeft
-        case (Right(a1), Right(a2)) => a.asRight
+  def apply(a: A)(implicit se: Semigroup[E]): Validated[E, A] = this match {
+    case Pure(f)   => f(a)
+    case And(l, r) => Semigroupal.map2(l(a), r(a))((_, _) => a)
+    case Or(l, r) =>
+      l(a) match {
+        case v @ Valid(_) => v
+        case Invalid(e1) =>
+          r(a) match {
+            case v @ Valid(_) => v
+            case Invalid(e2)  => Invalid(e1 |+| e2)
+          }
       }
   }
 }
 
-final case class Pure[E, A](chk: A => Either[E, A]) extends Check[E, A]
+final case class Pure[E, A](chk: A => Validated[E, A]) extends Check[E, A]
 final case class And[E, A](l: Check[E, A], r: Check[E, A]) extends Check[E, A]
+final case class Or[E, A](l: Check[E, A], r: Check[E, A]) extends Check[E, A]
 
 object DataValidation {}
