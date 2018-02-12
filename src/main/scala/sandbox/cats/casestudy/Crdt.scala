75ef4eee9fa0ef45b052df9f26cd6f04d1887387
Rolandas Valteris <rolandas.valteris@gmail.com>
2018-02-12 18:00:15 -0500

    case study 11. CRDTs - abstracted GCounter to typeclass

--- src/main/scala/sandbox/cats/casestudy/Crdt.scala
+++ src/main/scala/sandbox/cats/casestudy/Crdt.scala
@@ -21,22 +21,40 @@ object BoundedSemiLattice {
   }
 }
 
-final case class GCounter[D](counters: Map[String, D]) {
+trait GCounter[F[_, _], K, V] {
+  def increment(f: F[K, V])(k: K, v: V)(implicit m: Monoid[V]): F[K, V]
+  def merge(f1: F[K, V], f2: F[K, V])(implicit b: BoundedSemiLattice[V]): F[K, V]
+  def total(f: F[K, V])(implicit m: Monoid[V]): V
+}
 
-  import cats.syntax.semigroup._
+object GCounter {
 
-  def increment(machine: String, amount: D)(implicit m: Monoid[D]): GCounter[D] = {
-    val newAmount = counters.getOrElse(machine, m.empty) |+| amount
-    copy(counters = counters + (machine -> newAmount))
+  def apply[F[_, _], K, V](implicit c: GCounter[F, K, V]): GCounter[F, K, V] = c
+
+  implicit def mapGCounter[K, V]: GCounter[Map, K, V] = new GCounter[Map, K, V] {
+
+    import cats.syntax.semigroup._
+
+    override def increment(f: Map[K, V])(k: K, v: V)(implicit m: Monoid[V]): Map[K, V] = {
+      val newV = f.getOrElse(k, m.empty) |+| v
+      f + (k -> newV)
+    }
+
+    import cats.instances.map._
+
+    override def merge(f1: Map[K, V], f2: Map[K, V])(implicit b: BoundedSemiLattice[V]): Map[K, V] =
+      f1 |+| f2
+
+    import Crdt._
+    import cats.syntax.foldable._
+
+    override def total(f: Map[K, V])(implicit m: Monoid[V]): V = f.values.combineAll
   }
+}
 
-  import cats.instances.map._
-
-  def merge(that: GCounter[D])(implicit b: BoundedSemiLattice[D]): GCounter[D] =
-    GCounter(counters |+| that.counters)
+object Crdt {
 
   import cats.{Eval, Foldable}
-  import cats.syntax.foldable._
 
   implicit val iterableFoldable: Foldable[Iterable] = new Foldable[Iterable] {
     override def foldLeft[A, B](fa: Iterable[A], b: B)(f: (B, A) => B): B =
@@ -46,8 +64,4 @@ final case class GCounter[D](counters: Map[String, D]) {
         f: (A, Eval[B]) => Eval[B]): Eval[B] =
       Eval.now(fa.foldRight(lb.value)((a, b) => f(a, Eval.now(b)).value))
   }
-
-  def total(implicit m: Monoid[D]): D = counters.values.combineAll
 }
-
-object Crdt {}
