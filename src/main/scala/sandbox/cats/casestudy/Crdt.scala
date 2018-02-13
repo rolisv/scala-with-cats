d79229b96df730d011249874b6be859e0f3a1041
Rolandas Valteris <rolandas.valteris@gmail.com>
2018-02-13 16:57:07 -0500

    case study 11. CRDTs - abstracted key-value store into type-class

--- src/main/scala/sandbox/cats/casestudy/Crdt.scala
+++ src/main/scala/sandbox/cats/casestudy/Crdt.scala
@@ -21,6 +21,42 @@ object BoundedSemiLattice {
   }
 }
 
+trait KeyValueStore[F[_, _]] {
+  def get[K, V](f: F[K, V])(k: K): Option[V]
+  def getOrElse[K, V](f: F[K, V])(k: K, default: => V): V =
+    get(f)(k).getOrElse(default)
+
+  def put[K, V](f: F[K, V])(k: K, v: V): F[K, V]
+
+  def values[K, V](f: F[K, V]): Iterable[V]
+}
+
+object KeyValueStore {
+  implicit val mapInstance: KeyValueStore[Map] = new KeyValueStore[Map] {
+    override def values[K, V](f: Map[K, V]): Iterable[V] = f.values
+
+    override def put[K, V](f: Map[K, V])(k: K, v: V): Map[K, V] = f + (k -> v)
+
+    override def get[K, V](f: Map[K, V])(k: K): Option[V] = f.get(k)
+
+    override def getOrElse[K, V](f: Map[K, V])(k: K, default: => V): V = f.getOrElse(k, default)
+  }
+
+  implicit class KvsOps[F[_, _], K, V](val f: F[K, V]) extends AnyVal {
+    def get(k: K)(implicit kvs: KeyValueStore[F]): Option[V] =
+      kvs.get(f)(k)
+
+    def getOrElse(k: K, default: => V)(implicit kvs: KeyValueStore[F]): V =
+      kvs.getOrElse(f)(k, default)
+
+    def put(k: K, v: V)(implicit kvs: KeyValueStore[F]): F[K, V] =
+      kvs.put(f)(k, v)
+
+    def values(implicit kvs: KeyValueStore[F]): Iterable[V] =
+      kvs.values(f)
+  }
+}
+
 trait GCounter[F[_, _], K, V] {
   def increment(f: F[K, V])(k: K, v: V)(implicit m: Monoid[V]): F[K, V]
   def merge(f1: F[K, V], f2: F[K, V])(implicit b: BoundedSemiLattice[V]): F[K, V]
@@ -31,25 +67,26 @@ object GCounter {
 
   def apply[F[_, _], K, V](implicit c: GCounter[F, K, V]): GCounter[F, K, V] = c
 
-  implicit def mapGCounter[K, V]: GCounter[Map, K, V] = new GCounter[Map, K, V] {
+  implicit def gCounterInstance[F[_, _], K, V](implicit kvs: KeyValueStore[F],
+                                               km: Monoid[F[K, V]]): GCounter[F, K, V] =
+    new GCounter[F, K, V] {
+      import KeyValueStore._
+      import cats.syntax.semigroup._
 
-    import cats.syntax.semigroup._
+      override def increment(f: F[K, V])(k: K, v: V)(implicit m: Monoid[V]): F[K, V] = {
+        val newV = f.getOrElse(k, m.empty) |+| v
+        f.put(k, newV)
+      }
 
-    override def increment(f: Map[K, V])(k: K, v: V)(implicit m: Monoid[V]): Map[K, V] = {
-      val newV = f.getOrElse(k, m.empty) |+| v
-      f + (k -> newV)
+      override def merge(f1: F[K, V], f2: F[K, V])(implicit b: BoundedSemiLattice[V]): F[K, V] =
+        f1 |+| f2
+
+      import Crdt._
+      import cats.syntax.foldable._
+
+      override def total(f: F[K, V])(implicit m: Monoid[V]): V =
+        f.values.combineAll
     }
-
-    import cats.instances.map._
-
-    override def merge(f1: Map[K, V], f2: Map[K, V])(implicit b: BoundedSemiLattice[V]): Map[K, V] =
-      f1 |+| f2
-
-    import Crdt._
-    import cats.syntax.foldable._
-
-    override def total(f: Map[K, V])(implicit m: Monoid[V]): V = f.values.combineAll
-  }
 }
 
 object Crdt {
